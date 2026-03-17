55503cb743a3b8656efcb94b3ca0591197636f03
Albérico Eduardo <albericoeduardo202@gmail.com>
2026-03-17 08:04:50 -0300

    feat: design system module

---
+++ lib/design_system/constants/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+//Brand colors
+const Color primaryBrandColor = Color(0xffFFB347);
+const Color secondaryBrandColor = Color(0xffFFFFFF);
+
+//System colors
+const Color textColor = Color(0xff000000);
+const Color greenestTextColor = Color(0xff064E3B);
+const Color greenTextColor = Color(0xff065F46);
+const Color whiteGreenTextColor = Color(0xff047857);
+const Color inputFieldColor = Color.fromARGB(137, 0, 0, 0);
+const Color inputTextColor = Color.fromARGB(127, 0, 0, 0);
+const Color backgroundColor = Color(0xffF1F1F1);
+const Color normalErrorSystemColor = Color(0xffE03616);
